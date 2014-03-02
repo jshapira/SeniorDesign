71a50b68a0ec8bb84b1dc52a75caeec6e1c1a4bb
jshapira <jennieshapira@host-100-148.paphrad.clients.pavlovmedia.com>
2014-03-02 14:33:06 -0500

    Adding a bunch of files
    SRDSN Folder has Xcode project with small iOS app to interface with
    arduino.
    
    Sketches folder has various sketches for getting the IP, making simple
    movements, and interfering with the iOS app

---
+++ SRDSN/SRDSN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SRDSN
+//
+//  Created by Jennie Shapira on 2/27/14.
+//  Copyright (c) 2014 Jennie Shapira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *myWebView;
+
+
+@end
---
+++ SRDSN/SRDSN/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SRDSN
+//
+//  Created by Jennie Shapira on 2/27/14.
+//  Copyright (c) 2014 Jennie Shapira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
